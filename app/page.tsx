1612d997307fe29df60e8d8e8c308b2eda76a1bf
Nikita-singh-14 <nk2371174@gmail.com>
2026-08-19 22:29:21 +0530

    reduce home page font size for all device

--- app/page.tsx
+++ app/page.tsx
@@ -198,7 +198,7 @@ export default function Home() {
               A calmer kind of energy
             </div>
 
-            <h1 className="max-w-3xl text-2xl font-black leading-[0.92] tracking-tight md:text-7xl lg:text-8xl">
+            <h1 className="max-w-3xl text-2xl font-black leading-[0.92] tracking-tight md:text-6xl lg:text-6xl">
               What if your energy drink helped you{" "}
               <span className="text-[#f28c8c]">do nothing?</span>
             </h1>
